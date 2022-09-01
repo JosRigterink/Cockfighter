88e53e6618e63811a62dc0c9b72439d73d0f18fc
JosRigterink <rigterinkjos@gmail.com>
2022-09-01 13:38:40 +0200

    Main

---
+++ Cockfighter/Assets/scripts/PlayerManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    PhotonView pv;
+
+    GameObject controller;
+
+    void Awake()
+    {
+        pv = GetComponent<PhotonView>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (pv.IsMine)
+        {
+            CreateController();
+        }
+    }
+
+    void CreateController()
+    {
+        Transform spawnpoint = SpawnManager.Instance.GetSpawnPoint();
+        controller = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), spawnpoint.position, spawnpoint.rotation, 0, new object[] { pv.ViewID });
+    }
+
+    public void Die()
+    {
+        PhotonNetwork.Destroy(controller);
+        CreateController();
+    }
+
+}
