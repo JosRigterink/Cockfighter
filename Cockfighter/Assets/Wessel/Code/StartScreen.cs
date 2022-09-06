4dffd1ea5f21735d8629efb80adb33b1522fff36
Weezle0 <wesselbrattinga@gmail.com>
2022-09-06 14:01:06 +0200

    startscherm

---
+++ Cockfighter/Assets/Wessel/Code/StartScreen.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour
+{
+    public GameObject[] canvasses;
+
+    void Start()
+    {
+        canvasses[0].SetActive(true);
+        canvasses[1].SetActive(false);
+    }
+    public void PressAnywhere()
+    {
+        canvasses[0].SetActive(false);
+        canvasses[1].SetActive(true);
+    }
+}
