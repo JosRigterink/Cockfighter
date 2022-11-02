735c5e59fbc1343be2743c1a6c9483e292d4634a
JosRigterink <rigterinkjos@gmail.com>
2022-11-02 16:13:49 +0100

    werkend shop fix

--- Cockfighter/Assets/ShopManager.cs
+++ Cockfighter/Assets/ShopManager.cs
@@ -1,9 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class ShopManager : MonoBehaviour
 {
+    public string itemNameString;
+    public int itemPriceInt;
+
+    public Button skinMenuButton;
+    public Button shopMenuButton;
+
+    public int shopNumber;
+    public PurchaseManager manager;
+
+    public TMP_Text itemName;
+    public TMP_Text itemPrice;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +31,8 @@ public class ShopManager : MonoBehaviour
 
     public void Buy()
     {
-
+        manager.intje = shopNumber;
+        itemName.text = itemNameString;
+        itemPrice.text = itemPriceInt.ToString();
     }
 }
